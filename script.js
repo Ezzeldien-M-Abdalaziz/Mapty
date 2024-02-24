d6676ab2bd8bdb0db11ab4b3c8ef56f030598c90
Ezzeldien <ezzeldien.m.abdalziz@gmail.com>
2024-02-24 17:40:12 +0300

    event handler

--- script.js
+++ script.js
@@ -16,9 +16,31 @@ navigator.geolocation.getCurrentPosition(
     function(position) {
         const {latitude} = position.coords;
         const {longitude} = position.coords;
-        console.log(longitude,latitude);
+        const coords = [latitude,longitude];  //out coordinates
+        //console.log(coords);
+
+        // this code is from the documentation of the liberary to show the map
+        const map = L.map('map').setView(coords, 15);   //15 here is the num of zoom on the map  //'map' is an id with styling of the map
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {   //the theme of the map 
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+            map.on('click' , function(mapEvent){
+                const {lat,lng} = mapEvent.latlng;
+                L.marker({lat,lng}).addTo(map).bindPopup(L.popup({
+                    maxWidth:250,
+                    minWidth:100,
+                    autoClose:false,
+                    closeOnClick:false,
+                    className:'running-popup'
+                })
+                )
+                .setPopupContent('WorkOut')
+                .openPopup();
+            });
+        //end of code
     },
     function(){
         alert('ssxs');
-    }
+    } 
 );
